4ded160509b9fea05c513a668093e5867051d622
IndiraMG <40215375+IndiraMG@users.noreply.github.com>
2019-04-06 18:29:52 -0500

    Update simultree.R

--- simultree.R
+++ simultree.R
@@ -1,17 +1,17 @@
 ## This script was execute in sofware RStudio Version 1.1.463 
 
-setwd("~/Desktop/Post_2")		#set directory	
-library(ape)					#load library		
-numt<-c(12,48)					#vector with number of terminales	
+setwd("~/Desktop/Post_2") #set directory	
+library(ape) #load library		
+numt<-c(12,48) #vector with number of terminales	
 namet<-paste("T", 1:48, sep="") #vector with the names of the trees we will simulate
 
-for (j in 1:3) {				#loop for do 3 replicas
+for (j in 1:3) {	#loop for do 3 replicas
   
-   for (i in numt) {			#loop for apply function en the two types of trees (48 y 12)
+   for (i in numt) {	#loop for apply function en the two types of trees (48 and 12)
     
-     topos<-rtree(i,rooted = T, tip.label = namet[1:i], br=0.5)		#simulate each tree and save in vector
-     names<-paste("tre", i , j ,".tre", sep="")						#create names of trees and save in vector
-     write.tree(topos, names, digits=10)							#save trees in directory
+     topos<-rtree(i,rooted = T, tip.label = namet[1:i], br=0.5)	#simulate each tree and save in vector
+     names<-paste("tre", i , j ,".tre", sep="")	#create names of trees and save in vector
+     write.tree(topos, names, digits=10) #save trees in directory
     }
 
 }
